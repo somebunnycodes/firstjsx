23d271f5d4348e568bf15c3e08fb22ac63ec0d5a
Bunny Mueller <thebunnymueller@gmail.com>
2022-03-12 15:27:00 +0100

    initial commit

---
+++ src/App.js
@@ -0,0 +1,22 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <header className="App-header">
+      <h1>Hello Dojo!</h1>
+    
+      <h2>
+        Things I need to do:
+        <ul>
+          <li>Learn react</li>
+          <li>Climb Mt. Everest</li>
+          <li>Run a marathon</li>
+          <li>Feed the dogs</li>
+        </ul>
+      </h2>
+    </header>
+  );
+}
+
+export default App;
